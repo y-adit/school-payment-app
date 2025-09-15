6899f897e6a0d85d1a159108e2db2459526c6cf3
y-adit <adityayadav3004@gmail.com>
2025-09-15 12:55:36 +0530

    fix: disable frontend serving in backend

--- backend/index.js
+++ backend/index.js
@@ -29,13 +29,13 @@ app.use('/api/payment', paymentRoutes);
 app.use('/api/transactions', transactionRoutes);
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+//   });
+// }
 
 // Test route
 app.get('/', (req, res) => {
